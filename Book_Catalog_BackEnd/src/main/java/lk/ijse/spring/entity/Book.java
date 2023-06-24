f051f0524111d47610c989c6b27f245d561a4bc4
uwanitheekshani <uwanitheekshani@gmail.com>
2023-06-24 16:33:16 +0530

    added data jpa dependency

--- Book_Catalog_BackEnd/src/main/java/lk/ijse/spring/entity/Book.java
+++ Book_Catalog_BackEnd/src/main/java/lk/ijse/spring/entity/Book.java
@@ -1,13 +1,26 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
 /**
  * @author acer on 6/24/2023.
  * @project Book_Catalog_BackEnd
  */
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
 public class Book {
 
+    @Id
     private String bookId;
     private String category;
     private String title;

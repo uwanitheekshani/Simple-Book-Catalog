3b00ea7793155efc60a5980fe7b6fa581cb11bb3
uwanitheekshani <uwanitheekshani@gmail.com>
2023-06-24 16:10:31 +0530

    create bookDTO

--- Book_Catalog_BackEnd/src/main/java/lk/ijse/spring/entity/Book.java
+++ Book_Catalog_BackEnd/src/main/java/lk/ijse/spring/entity/Book.java
@@ -1,10 +1,17 @@
 package lk.ijse.spring.entity;
 
+import java.math.BigDecimal;
+
 /**
  * @author acer on 6/24/2023.
  * @project Book_Catalog_BackEnd
  */
 public class Book {
 
+    private String bookId;
+    private String category;
+    private String title;
+    private String author;
+    private BigDecimal price;
 
 }

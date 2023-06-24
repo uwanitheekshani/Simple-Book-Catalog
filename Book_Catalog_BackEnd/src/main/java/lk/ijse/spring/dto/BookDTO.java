db1858c595abbfdf519cf7fd2e25fb789cdaaaa3
uwanitheekshani <uwanitheekshani@gmail.com>
2023-06-24 16:36:52 +0530

    create book dto'

--- Book_Catalog_BackEnd/src/main/java/lk/ijse/spring/dto/BookDTO.java
+++ Book_Catalog_BackEnd/src/main/java/lk/ijse/spring/dto/BookDTO.java
@@ -1,8 +1,24 @@
 package lk.ijse.spring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+
 /**
  * @author acer on 6/24/2023.
  * @project Book_Catalog_BackEnd
  */
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class BookDTO {
+    private String bookId;
+    private String category;
+    private String title;
+    private String author;
+    private double price;
 }

eca862a17ec86acabe3b77374f3a6fc73181ba17
uwanitheekshani <uwanitheekshani@gmail.com>
2023-06-24 15:21:58 +0530

    added dependencies

--- Book_Catalog_BackEnd/src/main/java/lk/ijse/spring/WebAppInitializer.java
+++ Book_Catalog_BackEnd/src/main/java/lk/ijse/spring/WebAppInitializer.java
@@ -1,8 +1,23 @@
 package lk.ijse.spring;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 /**
  * @author acer on 6/24/2023.
  * @project Book_Catalog_BackEnd
  */
+
+@SpringBootApplication
 public class WebAppInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppInitializer.class);
+    }
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }

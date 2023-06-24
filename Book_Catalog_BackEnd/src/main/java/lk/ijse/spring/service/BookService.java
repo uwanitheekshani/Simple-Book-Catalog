9ba91bbf2835011b6e46bdf9bc25c593f1cee024
uwanitheekshani <uwanitheekshani@gmail.com>
2023-06-24 18:30:09 +0530

    added methods for service

--- Book_Catalog_BackEnd/src/main/java/lk/ijse/spring/service/BookService.java
+++ Book_Catalog_BackEnd/src/main/java/lk/ijse/spring/service/BookService.java
@@ -1,8 +1,22 @@
 package lk.ijse.spring.service;
 
+import lk.ijse.spring.dto.BookDTO;
+
+import java.util.ArrayList;
+
 /**
  * @author acer on 6/24/2023.
  * @project Book_Catalog_BackEnd
  */
 public interface BookService {
+
+    public void addBook(BookDTO dto);
+
+    public void deleteBook(String bookId);
+
+    public void updateBook(BookDTO dto);
+
+    public ArrayList<BookDTO> getAllBooks();
+
+    public BookDTO searchBookByTitle(String title);
 }
--- Book_Catalog_BackEnd/src/main/java/lk/ijse/spring/service/impl/BookServiceImpl.java
+++ Book_Catalog_BackEnd/src/main/java/lk/ijse/spring/service/impl/BookServiceImpl.java
@@ -1,8 +1,42 @@
 package lk.ijse.spring.service.impl;
 
+import lk.ijse.spring.dto.BookDTO;
+import lk.ijse.spring.service.BookService;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+
 /**
  * @author acer on 6/24/2023.
  * @project Book_Catalog_BackEnd
  */
-public class BookServiceImpl {
+
+@Service
+@Transactional
+public class BookServiceImpl implements BookService {
+    @Override
+    public void addBook(BookDTO dto) {
+
+    }
+
+    @Override
+    public void deleteBook(String bookId) {
+
+    }
+
+    @Override
+    public void updateBook(BookDTO dto) {
+
+    }
+
+    @Override
+    public ArrayList<BookDTO> getAllBooks() {
+        return null;
+    }
+
+    @Override
+    public BookDTO searchBookByTitle(String title) {
+        return null;
+    }
 }

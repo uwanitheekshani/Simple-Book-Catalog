dbe596787043701b7004a80da6b6847b84f6c5ab
uwanitheekshani <uwanitheekshani@gmail.com>
2023-06-25 02:24:50 +0530

    create userDTO

---
+++ Book_Catalog_BackEnd/src/main/java/lk/ijse/spring/dto/UserDTO.java
@@ -0,0 +1,8 @@
+package lk.ijse.spring.dto;
+
+/**
+ * @author acer on 6/25/2023.
+ * @project Book_Catalog_BackEnd
+ */
+public class UserDTO {
+}
--- Book_Catalog_BackEnd/src/main/java/lk/ijse/spring/entity/User.java
+++ Book_Catalog_BackEnd/src/main/java/lk/ijse/spring/entity/User.java
@@ -1,8 +1,25 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
 /**
  * @author acer on 6/25/2023.
  * @project Book_Catalog_BackEnd
  */
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
 public class User {
+    @Id
+    private String email;
+    private String password;
 }

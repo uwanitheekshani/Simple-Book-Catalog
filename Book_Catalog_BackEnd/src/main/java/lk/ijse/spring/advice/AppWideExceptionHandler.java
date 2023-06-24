064c3d0d2ae09c688f9589bc8ec21a2ddceb9870
uwanitheekshani <uwanitheekshani@gmail.com>
2023-06-24 15:54:47 +0530

    create response util

--- Book_Catalog_BackEnd/src/main/java/lk/ijse/spring/advice/AppWideExceptionHandler.java
+++ Book_Catalog_BackEnd/src/main/java/lk/ijse/spring/advice/AppWideExceptionHandler.java
@@ -1,8 +1,25 @@
 package lk.ijse.spring.advice;
 
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 /**
  * @author acer on 6/24/2023.
  * @project Book_Catalog_BackEnd
  */
+
+@RestControllerAdvice
+@CrossOrigin
 public class AppWideExceptionHandler {
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseUtil handleException(RuntimeException e){
+
+        return new ResponseUtil("500",e.getMessage(),null);
+    }
 }
